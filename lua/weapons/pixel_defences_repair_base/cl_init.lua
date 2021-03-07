0f44e3423bee4b2720f3bfd09a3a65c031abf951
Tom O'Sullivan <tom@tomdotbat.dev>
2021-03-07 19:50:41 +0000

    Redo the UI for the repair tool

--- lua/weapons/pixel_defences_repair_base/cl_init.lua
+++ lua/weapons/pixel_defences_repair_base/cl_init.lua
@@ -1,49 +1,69 @@
 
 include("shared.lua")
 
+SWEP.NextSpark = 0
+
 function SWEP:PrimaryAttack() end
+function SWEP:SecondaryAttack() end
+
+PIXEL.RegisterFont("Defences.RepairType", "Open Sans Bold", 40)
+PIXEL.RegisterFont("Defences.RepairHealth", "Open Sans SemiBold", 34)
+
+function SWEP:DrawHUD()
+	local owner = self:GetOwner()
+
+	local lookingAtEnt = PIXEL.Defences.GetLookingAtEnt(owner)
+	if not lookingAtEnt then return end
+
+	self.CurHealth = Lerp(FrameTime() * 8, self.CurHealth, lookingAtEnt:GetDefenceHealth())
+
+	local centerW = ScrW() * .5
+	local textX = ScrH() * .8
+
+	local _, textH = PIXEL.DrawSimpleText(
+		"Tier " .. lookingAtEnt.DefenceTier .. " Defence",
+		"Defences.EditorType", centerW, textX,
+		PIXEL.Colors.PrimaryText, TEXT_ALIGN_CENTER
+	)
+	textX = textX + textH
+
+	_, textH = PIXEL.DrawSimpleText(
+		"Health: " .. math.Round(self.CurHealth),
+		"Defences.EditorRotation", centerW, textX,
+		PIXEL.LerpColor(self.CurHealth / lookingAtEnt.DefenceMaxHP, PIXEL.Colors.Negative, PIXEL.Colors.Positive), TEXT_ALIGN_CENTER
+	)
+
+	if CurTime() < self.NextSpark then return end
+	if not input.IsMouseDown(MOUSE_LEFT) or (lookingAtEnt:GetDefenceHealth() >= lookingAtEnt.DefenceMaxHP) then return end
+
+	local tr = owner:GetEyeTrace()
+	local effectdata = EffectData()
+
+	effectdata:SetOrigin(tr.HitPos)
+	effectdata:SetMagnitude(0.5)
+	effectdata:SetScale(2)
+	effectdata:SetRadius(3)
+
+	util.Effect("Sparks", effectdata)
+
+	self.NextSpark = CurTime() + .1
+	sound.Play("ambient/energy/spark" .. math.random(1, 6) .. ".wav", tr.HitPos, 80, 100, 1)
+end
 
 local laserMat = Material("sprites/bluelaser1")
-function SWEP:DrawHUD()
-	local ply = LocalPlayer()
-	local facingEnt = PIXEL.Defences.GetLookingAtEnt(ply)
-	if (!facingEnt) then return end
+local laserCol = Color(255, 0, 0)
+function SWEP:ViewModelDrawn()
+	local owner = self:GetOwner()
+	if not IsValid(owner) then return end
 
-	self.CurHealth = Lerp(FrameTime() * 5, self.CurHealth, facingEnt:GetDefenceHealth())
+	local viewModel = owner:GetViewModel()
+	if not IsValid(viewModel) then return end
 
-	local col = Color(255, 255 * (self.CurHealth / facingEnt.DefenceMaxHP), 255 * (self.CurHealth / facingEnt.DefenceMaxHP))
+	local _, endPos = PIXEL.Defences.GetLookingAtEnt(owner)
+	if not endPos then return end
 
-	local text = "Tier " .. facingEnt.DefenceTier .. " Defence"
-	surface.SetFont("oglDefenseHudTitle")
-	surface.SetTextColor(col)
-	local w,h = surface.GetTextSize(text)
-	surface.SetTextPos((ScrW()/2)-(w/2), (ScrH()*0.99)-150)
-	surface.DrawText(text)
-	draw.RoundedBox(0, (ScrW()/2)-(w/2), (ScrH()*0.99)-h*2, w, 5, col)
-
-	text = "Health: " .. math.Round(self.CurHealth)
-	surface.SetFont("oglDefenseHudSubTitle")
-	w,h = surface.GetTextSize(text)
-	surface.SetTextPos((ScrW()/2)-(w/2), (ScrH()*0.99)-90)
-	surface.DrawText(text)
-
-    local vm = ply:GetViewModel()
-	if vm then
-		local t = util.GetPlayerTrace(ply)
-		local tr = util.TraceLine(t)
-
-	    cam.Start3D(EyePos(), EyeAngles())
-			render.SetMaterial(laserMat)
-			render.DrawBeam(vm:GetAttachment("1").Pos, tr.HitPos, 4, 0, 12.5, Color(0, 255, 0, 255))
-		cam.End3D()
-
-		if (input.IsMouseDown(MOUSE_LEFT) and (facingEnt:GetDefenceHealth() < facingEnt.DefenceMaxHP)) then
-			local effectdata = EffectData()
-			effectdata:SetOrigin(tr.HitPos)
-			effectdata:SetMagnitude(1)
-			effectdata:SetScale(0.5)
-			effectdata:SetRadius(2)
-			util.Effect("Sparks", effectdata)
-		end
-	end
+	cam.Start3D()
+		render.SetMaterial(laserMat)
+		render.DrawBeam(viewModel:GetAttachment("1").Pos, endPos, 4, 0, 12.5, laserCol)
+	cam.End3D()
 end
